a270204aaf1a4c260469fc33e0f0f257347cb83b
DannyGardner <dannygardnercg@gmail.com>
2022-02-09 09:55:09 -0600

    header image, started creating create form

--- src/components/CreateTrip.js
+++ src/components/CreateTrip.js
@@ -1,21 +1,69 @@
 import React from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default function CreateTrip() {
   return (
+    <div className='create-container'>
+
+      <div className='create-form-container'>
+    <Form>
+      <FormGroup>
+        <Label for="exampleEmail">
+          <h2>Create a new trip</h2>
+        </Label>
+       </FormGroup>
+      <FormGroup>
+        <Label for="exampleEmail">
+          Trip Name
+        </Label>
+        <Input
+          id="tripname"
+          name="trip"
+          placeholder="trip name"
+          type="text"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="examplePassword">
+          Budget
+        </Label>
+        <Input
+          id="budget"
+          name="budget"
+          placeholder="budget"
+          type="text"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleNumber">
+          Number
+        </Label>
+        <Input
+          id="exampleNumber"
+          name="number"
+          placeholder="number placeholder"
+          type="number"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleRange">
+          Range
+        </Label>
+        <Input
+          id="exampleRange"
+          name="range"
+          type="range"
+        />
+      </FormGroup>
+    </Form>
+    </div>
     <div>
-      <div>
-        <div>Current Trip Test</div>
+        <div className='create-totals-container'>
+          <h4>Food</h4>
+          <h4>Transportation</h4>
+          <h4>Lodging</h4>
+        </div>
       </div>
-      <form>
-        <div className='add-item'>
-          Food:
-          <input name="food"></input>Cost:<input name="cost"></input><button>+</button>
-        </div>
-        <div className='add-item'>
-          Lodging:
-          <input name="food"></input>Cost:<input name="cost"></input><button>+</button>
-        </div>
-      </form>
     </div>
   )
 }
