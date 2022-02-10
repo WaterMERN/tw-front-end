c6de3d0c6f3ead9e6766d30930daf9de2f466e59
DannyGardner <dannygardnercg@gmail.com>
2022-02-10 11:11:09 -0600

    updated new components

---
+++ src/components/AddExpenseItem.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { List } from 'reactstrap'
+import { useState } from 'react'
+import ExpenseListItem from './ExpenseListItem'
+
+function AddExpenseItem() {
+    const [list, setList] = useState([{
+        food: 'test'
+    }])
+    const [newItem, setnewItem] = useState([])
+
+    // expenseItem State {
+    //     category: from drop 
+    //     Title: from input field
+    //     cost: from input 
+    //   } // to be answered shortly 
+    // change to drop down list for expense categories to add to expense list state 
+
+    const handleChange = (event) => {
+        // const { name, value } = event.target
+        setnewItem(
+            event.target.value
+        )
+        console.log(newItem)
+    }
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        const newThing = {
+            food: newItem
+        }
+        setList([ ...list, newThing ])
+        console.log(list)
+    }
+    return (
+        <div>
+            <div>BudgetList</div>
+
+            <div>
+                <div for="exampleNumber">
+                    Food
+                </div>
+                <input
+                    onChange={handleChange}
+                    value={list.food}
+                    name="food"
+                    type="text" 
+                    placeholder="food item"
+                />
+                <button onClick={handleClick} type="submit">Add</button>
+                <div className='list-container'>
+               {/* MOVE TO EXPENSE LIST COMPONENT */}
+                {list.map(item => {
+                    return (
+                        // <div>{item.food}</div>
+                        <ExpenseListItem list={item}/>
+                    )
+                }
+                )}
+                </div> 
+            </div>
+        </div>
+    )
+}
+
+export default AddExpenseItem
